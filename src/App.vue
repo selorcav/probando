9dc898a8f00b3af53339b84e72e4b12019353239
Sebastian Lorca Vargas <slorca@pcfactory.intranet>
2023-01-25 18:14:13 -0300

    probando

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div v-for="item in valor">
+      {{ item.title }}
+      <SoyUnComponente :api="item" />
+      <hr />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import SoyUnComponente from "@/components/SoyUnComponente.vue";
+
+export default {
+  components: {
+    SoyUnComponente,
+  },
+
+  data() {
+    return {
+      valor: [],
+    };
+  },
+  methods: {
+    consumirApi: function (api) {
+      axios
+        .get(api)
+        .then((res) => {
+          this.valor = res.data;
+          console.log(this.valor)
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+  },
+  created: function () {
+    this.consumirApi("https://stoplight.io/mocks/modyo/pcfactory-documentation/79796109/campanas/ofertas-videojuegos");
+  },
+  mounted: function () {},
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/SoyUnComponente.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <div v-for="separador in caracte">
+            <div v-for="item in separador.items ">
+                {{ item.marca }}
+                {{ item.id }}
+                <img :src="`https://www.pcfactory.cl/${item.thumbnail}`" alt="">
+            </div>
+            <!-- {{ separador  }} -->
+        </div>
+        <!-- <h2>{{ api.name }}</h2> -->
+        <div>
+            <!-- <h4>{{ caracte.name }}</h4>
+            <img :src="caracte.sprites.front_default"  alt=""> -->
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+    export default {
+        data() {
+            return {
+                caracte: []
+            }
+        },
+        props: { api: Object},
+        methods: {
+            consumirDatos: function (){
+                // console.log(`https://pokeapi.co/api/v2/pokemon/${this.api.name}`)
+
+                axios
+                .get(`https://stoplight.io/mocks/modyo/pcfactory-documentation/79796109/campanas/ofertas-videojuegos/${this.api.dataSource.content}`)
+                .then((res) => {
+                    console.log(res.data)
+                this.caracte = res.data;
+                })
+                .catch((e) => {
+                console.log(e);
+                });
+
+            }
+        },
+        created: function () {
+            this.consumirDatos();
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
